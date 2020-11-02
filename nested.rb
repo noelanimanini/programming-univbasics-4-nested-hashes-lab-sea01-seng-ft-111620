4e272279c2f661f6e784dbb85ee271bc1d70cc9c
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-02 06:40:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -62,13 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -86,13 +77,10 @@ def adding_matz
       :yukihiro_matsumoto => {
          :known_for => "Ruby",
          :languages => ["LISP", "C"]
-    }
+    }}
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable.
-  # return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
