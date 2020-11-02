9cdf6296319d3c4e04e5d405e159a6f276fe6c1c
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-02 06:38:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,7 +133,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-  programmer_hash
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 
 end
