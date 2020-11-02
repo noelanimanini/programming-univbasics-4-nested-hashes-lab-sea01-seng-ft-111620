32c375ce9b5f44a6d0437a154eb6ef21c1d2fee7
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-02 06:37:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,9 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       },
-      programmer_hash[:yukihiro_matsumoto] =
-      {:known_for => "Ruby", :languages => "LISP", "C" }
+      :yukihiro_matsumoto => {
+         :known_for => "Ruby",
+         :languages => ["LISP", "C"]
     }
 end
 
