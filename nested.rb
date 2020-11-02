bdf0b73859bebf2eb5fa421a693f9fcf095fbfa2
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-02 06:29:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash = [:grace_hopper]
 
 end
 
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash = [:alan_kay][:known_for]
 
 end
 
@@ -55,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash = [:dennis_ritchie][:languages]
 
 end
 
